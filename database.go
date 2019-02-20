d64ec52e407d73d2bec77380783c141344435746
Scot Bontrager <scot@indievisible.org>
2019-02-20 09:26:16 -0600

    split model.go into parts

--- database.go
+++ database.go
@@ -39,7 +39,7 @@ func Connect(uri string) error {
 		Log.Noticef("Setting up `documents` table...")
 		_, err := db.Exec(`CREATE TABLE documents (
             id varchar(64) PRIMARY KEY,
-			uploader varchar(32) NULL DEFAULT NULL,
+            uploader varchar(32) NULL DEFAULT NULL,
             content longblob NOT NULL,
             upload datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
             expiration datetime NULL DEFAULT NULL,
@@ -55,7 +55,7 @@ func Connect(uri string) error {
 	if table == "" {
 		Log.Noticef("Setting up `user` table...")
 		_, err := db.Exec(`CREATE TABLE user(
-			gid varchar(32) PRIMARY KEY,
+            gid varchar(32) PRIMARY KEY,
             iname varchar(64) NULL DEFAULT NULL,
             lockey varchar(64) NULL DEFAULT NULL
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin`)
@@ -69,7 +69,7 @@ func Connect(uri string) error {
 	if table == "" {
 		Log.Noticef("Setting up `locations` table...")
 		_, err := db.Exec(`CREATE TABLE locations(
-			gid varchar(32) PRIMARY KEY,
+            gid varchar(32) PRIMARY KEY,
             upTime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
             loc POINT
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin`)
@@ -84,9 +84,9 @@ func Connect(uri string) error {
 	if table == "" {
 		Log.Noticef("Setting up `tags` table...")
 		_, err := db.Exec(`CREATE TABLE tags(
-			tagID varchar(64) PRIMARY KEY,
-			owner varchar(32) NOT NULL,
-			name varchar(64) NULL DEFAULT NULL
+            tagID varchar(64) PRIMARY KEY,
+            owner varchar(32) NOT NULL,
+            name varchar(64) NULL DEFAULT NULL
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin`)
 		if err != nil {
 			return err
@@ -98,11 +98,11 @@ func Connect(uri string) error {
 	if table == "" {
 		Log.Noticef("Setting up `usertags` table...")
 		_, err := db.Exec(`CREATE TABLE usertags(
-			tagID varchar(64) NOT NULL,
-			gid varchar(32) NOT NULL,
-			state ENUM('Off', 'On') NOT NULL DEFAULT 'Off', 
+            tagID varchar(64) NOT NULL,
+            gid varchar(32) NOT NULL,
+            state ENUM('Off', 'On') NOT NULL DEFAULT 'Off', 
             color varchar(32) NOT NULL DEFAULT "FF5500",
-			PRIMARY KEY (tagID, gid)
+            PRIMARY KEY (tagID, gid)
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin`)
 		if err != nil {
 			return err
---
+++ model_user.go
@@ -0,0 +1,152 @@
+package PhDevBin
+
+import (
+	"database/sql"
+)
+
+// user stuff
+type UserData struct {
+	IngressName string
+	LocationKey string
+	Tags        []struct {
+		Id    string
+		Name  string
+		State string
+	}
+	OwnedTags []struct {
+		Name string
+		Tag  string
+	}
+}
+
+func InsertOrUpdateUser(id string, name string) error {
+	var tmpName = "Agent_" + id[:5]
+	lockey, err := GenerateSafeName()
+	_, err = db.Exec("INSERT INTO user VALUES (?,?,?) ON DUPLICATE KEY UPDATE gid = ?", id, tmpName, lockey, id)
+	if err != nil {
+		Log.Notice(err)
+		return err
+	}
+	_, err = db.Exec("INSERT INTO locations VALUES (?,NOW(),POINT(0,0)) ON DUPLICATE KEY UPDATE upTime = NOW()", id)
+	if err != nil {
+		Log.Notice(err)
+	}
+	return err
+}
+
+func SetIngressName(id string, name string) error {
+	_, err := db.Exec("UPDATE user SET iname = ? WHERE gid = ?", name, id)
+	if err != nil {
+		Log.Notice(err)
+	}
+	return err
+}
+
+func RemoveUserFromTag(id string, tag string) error {
+	_, err := db.Exec("DELETE FROM usertags WHERE gid = ? AND tagID = ?", tag, id)
+	if err != nil {
+		Log.Notice(err)
+	}
+	return err
+}
+
+func SetUserTagState(id string, tag string, state string) error {
+	if state != "On" {
+		state = "Off"
+	}
+	_, err := db.Exec("UPDATE usertags SET state = ? WHERE gid = ? AND tagID = ?", state, id, tag)
+	if err != nil {
+		Log.Notice(err)
+	}
+	return err
+}
+
+func GetUserData(id string, ud *UserData) error {
+	var in, lc sql.NullString
+
+	row := db.QueryRow("SELECT iname, lockey FROM user WHERE gid = ?", id)
+	err := row.Scan(&in, &lc)
+	if err != nil {
+		Log.Notice(err)
+		return err
+	}
+
+	// convert from sql.NullString to string in the struct
+	if in.Valid {
+		ud.IngressName = in.String
+	}
+	if lc.Valid {
+		ud.LocationKey = lc.String
+	}
+
+	var tagID, name, state sql.NullString
+	var tmp struct {
+		Id    string
+		Name  string
+		State string
+	}
+
+	rows, err := db.Query("SELECT t.tagID, t.name, x.state "+
+		"FROM tags=t, usertags=x "+
+		"WHERE x.gid = ? AND x.tagID = t.tagID", id)
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&tagID, &name, &state)
+		if err != nil {
+			Log.Error(err)
+			return err
+		}
+		if tagID.Valid {
+			tmp.Id = tagID.String
+		} else {
+			tmp.Id = ""
+		}
+		if name.Valid {
+			tmp.Name = name.String
+		} else {
+			tmp.Name = ""
+		}
+		if state.Valid {
+			tmp.State = state.String
+		} else {
+			tmp.State = ""
+		}
+		ud.Tags = append(ud.Tags, tmp)
+	}
+
+	var tmpO struct {
+		Name string
+		Tag  string
+	}
+	rowsO, err := db.Query("SELECT tagID, name FROM tags WHERE owner = ?", id)
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	defer rowsO.Close()
+	for rowsO.Next() {
+		err := rowsO.Scan(&tagID, &name)
+		if err != nil {
+			Log.Error(err)
+			return err
+		}
+		if tagID.Valid {
+			tmpO.Tag = tagID.String
+		} else {
+			tmpO.Tag = ""
+		}
+		if name.Valid {
+			tmpO.Name = name.String
+		} else {
+			tmpO.Name = ""
+		}
+		ud.OwnedTags = append(ud.OwnedTags, tmpO)
+	}
+
+	return nil
+}
