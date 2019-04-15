1adf17702591c20578e55a4d0398327208c2ca4c
Scot Bontrager <scot@indievisible.org>
2019-04-14 19:50:26 -0500

    finish the obvious v tests

--- model_venlone_test.go
+++ model_venlone_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 	"os"
+	"strconv"
 	"github.com/cloudkucooland/WASABI"
 )
 
@@ -19,7 +20,7 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestEasy(t *testing.T) {
+func TestConfigured(t *testing.T) {
 	b := WASABI.GetvEnlOne()
 	if b != true {
 		t.Errorf("V API Key not configured")
@@ -41,5 +42,34 @@ func TestVsearch(t *testing.T) {
 
 	if v.Data.Agent != "deviousness" {
 		t.Errorf("V agent found: %s", v.Status)
+	}
+}
+
+func TestStatusLocation(t *testing.T) {
+	gid := WASABI.GoogleID("118281765050946915735")
+
+	lat, lon, err := gid.StatusLocation()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	var fLat, fLon float64
+	fLat, _ = strconv.ParseFloat(lat, 64)
+	fLon, _ = strconv.ParseFloat(lon, 64)
+	if fLat > 90.0 || fLat < -90.0 {
+		t.Errorf("impossible lat: %f", fLat)
+	}
+	if fLon > 180.0 || fLon < -180.0 {
+		t.Errorf("impossible lon: %f", fLon)
+	}
+}
+
+func TestGid(t *testing.T) {
+	eid := WASABI.EnlID("23e27f48a04e55d6ae89188d3236d769f6629718")
+	gid, err := eid.Gid()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if gid.String() != "118281765050946915735" {
+		t.Errorf("EnlID(%s) = Gid(%s); expecting Gid(118281765050946915735)",eid.String(), gid.String())
 	}
 }
