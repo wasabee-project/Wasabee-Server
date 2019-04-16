8cd146fe4ce7c362f328f201fab1b7467b12b170
Scot Bontrager <scot@indievisible.org>
2019-04-16 09:31:02 -0500

    move testing things to more sane locations

---
+++ model_team_test.go
@@ -0,0 +1,67 @@
+package wasabi_test
+
+import (
+	"github.com/cloudkucooland/WASABI"
+	"testing"
+)
+
+func TestNewTeam(t *testing.T) {
+	gid := wasabi.GoogleID("118281765050946915735")
+	teamID, err := gid.NewTeam("Test Team")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	// fmt.Printf("TeamID: %s", teamID.String())
+	q, err := gid.AgentInTeam(teamID, false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if q == false {
+		t.Error("team creator not in team")
+	}
+	err = teamID.Rename("maeT tseT")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	var td wasabi.TeamData
+	err = teamID.FetchTeam(&td, true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	err = teamID.FetchTeam(&td, false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	err = gid.SetTeamState(teamID, "Off")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	err = gid.SetTeamState(teamID, "On")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	err = gid.SetTeamState(teamID, "Primary")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	p, err := gid.PrimaryTeam()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if p != teamID.String() {
+		t.Errorf("Primary team test fail: %s / %s", p, teamID.String())
+	}
+
+	err = gid.SetTeamState(teamID, "Wombat")
+	if err == nil {
+		t.Error("SetTeamState did not return an error on a bad value")
+	}
+
+	err = teamID.Delete()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	return
+}
--- model_venlone_test.go
+++ model_venlone_test.go
@@ -3,33 +3,11 @@ package wasabi_test
 import (
 	"fmt"
 	"github.com/cloudkucooland/WASABI"
-	"github.com/op/go-logging"
-	"os"
 	"strconv"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
-	wasabi.SetLogLevel(logging.DEBUG)
-	err := wasabi.Connect(os.Getenv("DATABASE"))
-	if err != nil {
-		wasabi.Log.Error(err)
-	}
-	wasabi.SetVEnlOne(os.Getenv("VENLONE_API_KEY"))
-
-	// flag.Parse()
-	exitCode := m.Run()
-	os.Exit(exitCode)
-}
-
-func TestLoadWordsFile(t *testing.T) {
-	err := wasabi.LoadWordsFile("eff_large_wordlist.txt")
-	if err != nil {
-		t.Error(err.Error())
-	}
-}
-
-func TestConfigured(t *testing.T) {
+func TestVConfigured(t *testing.T) {
 	b := wasabi.GetvEnlOne()
 	if b != true {
 		t.Errorf("V API Key not configured")
