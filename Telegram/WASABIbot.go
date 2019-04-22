80beb601098f23da50711d52e8c8aa0c74970b7e
Scot Bontrager <scot@indievisible.org>
2019-04-22 09:48:47 -0500

    split this into more files, make it easier to manage

--- Telegram/WASABIbot.go
+++ Telegram/WASABIbot.go
@@ -1,14 +1,11 @@
 package wasabitelegram
 
 import (
-	"bytes"
 	// "encoding/json"
 	"errors"
 	"fmt"
 	"github.com/cloudkucooland/WASABI"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"io/ioutil"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"text/template"
@@ -94,7 +91,6 @@ func WASABIBot(init TGConfiguration) error {
 }
 
 func runUpdate(update tgbotapi.Update) error {
-	var err error
 	if update.CallbackQuery != nil {
 		msg, err := callback(&update)
 		if err != nil {
@@ -106,55 +102,53 @@ func runUpdate(update tgbotapi.Update) error {
 		return nil
 	}
 
-	// callbacks have been processed, it must be a message, right?
-	if update.Message == nil {
-		return nil
-	}
+	if update.Message != nil {
+		// XXX move more of this into message() ?
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		defaultReply, err := templateExecute("default", update.Message.From.LanguageCode, nil)
+		if err != nil {
+			wasabi.Log.Error(err)
+			return err
+		}
+		msg.Text = defaultReply
+		msg.ParseMode = "MarkDown"
+		// s, _ := json.MarshalIndent(msg, "", "  ")
+		// wasabi.Log.Debug(string(s))
 
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-	defaultReply, err := templateExecute("default", update.Message.From.LanguageCode, nil)
-	if err != nil {
-		wasabi.Log.Error(err)
-		return err
-	}
-	msg.Text = defaultReply
-	msg.ParseMode = "MarkDown"
-	// s, _ := json.MarshalIndent(msg, "", "  ")
-	// wasabi.Log.Debug(string(s))
+		tgid := wasabi.TelegramID(update.Message.From.ID)
+		gid, verified, err := tgid.GidV()
+		if err != nil {
+			wasabi.Log.Error(err)
+			return err
+		}
 
-	tgid := wasabi.TelegramID(update.Message.From.ID)
-	gid, verified, err := tgid.GidV()
-	if err != nil {
-		wasabi.Log.Error(err)
-		return err
-	}
-
-	if gid == "" {
-		wasabi.Log.Debugf("unknown user: %s (%s); initializing", update.Message.From.UserName, string(update.Message.From.ID))
-		fgid, err := runRocks(tgid)
-		if fgid != "" && err == nil {
-			tmp, _ := templateExecute("InitTwoSuccess", update.Message.From.LanguageCode, nil)
-			msg.Text = tmp
-		} else {
-			err = newUserInit(&msg, &update)
+		if gid == "" {
+			wasabi.Log.Debugf("unknown user: %s (%s); initializing", update.Message.From.UserName, string(update.Message.From.ID))
+			fgid, err := runRocks(tgid)
+			if fgid != "" && err == nil {
+				tmp, _ := templateExecute("InitTwoSuccess", update.Message.From.LanguageCode, nil)
+				msg.Text = tmp
+			} else {
+				err = newUserInit(&msg, &update)
+				if err != nil {
+					wasabi.Log.Error(err)
+				}
+			}
+		} else if !verified {
+			wasabi.Log.Debugf("unverified user: %s (%s); verifying", update.Message.From.UserName, string(update.Message.From.ID))
+			err = newUserVerify(&msg, &update)
 			if err != nil {
 				wasabi.Log.Error(err)
 			}
+		} else { // verified user, process message
+			if err = message(&msg, &update, gid); err != nil {
+				wasabi.Log.Error(err)
+			}
 		}
-	} else if !verified {
-		wasabi.Log.Debugf("unverified user: %s (%s); verifying", update.Message.From.UserName, string(update.Message.From.ID))
-		err = newUserVerify(&msg, &update)
-		if err != nil {
-			wasabi.Log.Error(err)
-		}
-	} else { // verified user, process message
-		if err = message(&msg, &update, gid); err != nil {
-			wasabi.Log.Error(err)
-		}
-	}
 
-	bot.Send(msg)
-	bot.DeleteMessage(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
+		bot.Send(msg)
+		bot.DeleteMessage(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
+	}
 
 	return nil
 }
@@ -207,75 +201,6 @@ func newUserVerify(msg *tgbotapi.MessageConfig, inMsg *tgbotapi.Update) error {
 	return err
 }
 
-func templates(t map[string]*template.Template) error {
-	if config.FrontendPath == "" {
-		err := errors.New("FrontendPath not configured")
-		wasabi.Log.Critical(err)
-		return err
-	}
-
-	frontendPath, err := filepath.Abs(config.FrontendPath)
-	if err != nil {
-		wasabi.Log.Critical("Frontend path couldn't be resolved.")
-		panic(err)
-	}
-	config.FrontendPath = frontendPath
-
-	wasabi.Log.Debugf("Building Template function map")
-	funcMap := template.FuncMap{
-		"TGGetBotName": wasabi.TGGetBotName,
-		"TGGetBotID":   wasabi.TGGetBotID,
-		"TGRunning":    wasabi.TGRunning,
-		"Webroot":      wasabi.GetWebroot,
-		"WebAPIPath":   wasabi.GetWebAPIPath,
-		"VEnlOne":      wasabi.GetvEnlOne,
-	}
-	config.templateSet = make(map[string]*template.Template)
-
-	if err != nil {
-		wasabi.Log.Error(err)
-	}
-	wasabi.Log.Info("Including frontend telegram templates from: ", config.FrontendPath)
-	files, err := ioutil.ReadDir(config.FrontendPath)
-	if err != nil {
-		wasabi.Log.Error(err)
-	}
-
-	for _, f := range files {
-		lang := f.Name()
-		if f.IsDir() && len(lang) == 2 {
-			config.templateSet[lang] = template.New("").Funcs(funcMap) // one funcMap for all languages
-			// load the masters
-			config.templateSet[lang].ParseGlob(config.FrontendPath + "/master/*.tg")
-			// overwrite with language specific
-			config.templateSet[lang].ParseGlob(config.FrontendPath + "/" + lang + "/*.tg")
-			wasabi.Log.Debugf("Templates for lang [%s] %s", lang, config.templateSet[lang].DefinedTemplates())
-		}
-	}
-
-	return nil
-}
-
-func templateExecute(name, lang string, data interface{}) (string, error) {
-	if lang == "" {
-		lang = "en"
-	}
-
-	_, ok := config.templateSet[lang]
-	if !ok {
-		lang = "en" // default to english if the map doesn't exist
-	}
-
-	// s, _ := json.MarshalIndent(&data, "", "\t")
-	// wasabi.Log.Debugf("Calling template %s[%s] with data %s", name, lang, string(s))
-	var tpBuffer bytes.Buffer
-	if err := config.templateSet[lang].ExecuteTemplate(&tpBuffer, name, data); err != nil {
-		wasabi.Log.Notice(err)
-		return "", err
-	}
-	return tpBuffer.String(), nil
-}
-
 func keyboards(c *TGConfiguration) error {
 	c.baseKbd = tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
@@ -290,42 +215,6 @@ func keyboards(c *TGConfiguration) error {
 	)
 
 	return nil
-}
-
-func teamKeyboard(gid wasabi.GoogleID) (tgbotapi.InlineKeyboardMarkup, error) {
-	var ud wasabi.AgentData
-	var rows [][]tgbotapi.InlineKeyboardButton
-
-	if err := gid.GetAgentData(&ud); err == nil {
-		var i int
-		for _, v := range ud.Teams {
-			i++
-			var row []tgbotapi.InlineKeyboardButton
-			var on, off, primary tgbotapi.InlineKeyboardButton
-			if v.State == "Off" {
-				on = tgbotapi.NewInlineKeyboardButtonData("Activate "+v.Name, "team/activate/"+v.ID)
-				row = append(row, on)
-			}
-			if v.State == "On" || v.State == "Primary" {
-				off = tgbotapi.NewInlineKeyboardButtonData("Deactivate "+v.Name, "team/deactivate/"+v.ID)
-				row = append(row, off)
-			}
-			if v.State == "On" {
-				primary = tgbotapi.NewInlineKeyboardButtonData("Make "+v.Name+" Primary", "team/primary/"+v.ID)
-				row = append(row, primary)
-			}
-			rows = append(rows, row)
-
-			if i > 8 { // too many rows and the screen fills up
-				break
-			}
-		}
-	}
-
-	tmp := tgbotapi.InlineKeyboardMarkup{
-		InlineKeyboard: rows,
-	}
-	return tmp, nil
 }
 
 // This is where command processing takes place
@@ -358,85 +247,6 @@ func message(msg *tgbotapi.MessageConfig, inMsg *tgbotapi.Update, gid wasabi.Goo
 		msg.Text = "Location Processed"
 	}
 
-	return nil
-}
-
-// callback is where to determine which callback is called, and what to do with it
-func callback(update *tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
-	tgid := wasabi.TelegramID(update.CallbackQuery.From.ID)
-	gid, _, err := tgid.GidV()
-	if err != nil {
-		wasabi.Log.Error(err)
-		return msg, err
-	}
-
-	// s, _ := json.MarshalIndent(update.CallbackQuery, "", " " )
-	// wasabi.Log.Debug(string(s))
-
-	var resp tgbotapi.APIResponse
-	if update.CallbackQuery.Message.Chat.Type != "private" {
-		wasabi.Log.Errorf("Not in private chat: %s", update.CallbackQuery.Message.Chat.Type)
-		return msg, nil
-	}
-	lang := update.CallbackQuery.Message.From.LanguageCode
-
-	command := strings.SplitN(update.CallbackQuery.Data, "/", 3)
-	switch command[0] {
-	case "team":
-		_ = callbackTeam(command[1], command[2], gid, lang, &msg)
-		resp, err = bot.AnswerCallbackQuery(
-			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Team Updated", ShowAlert: false},
-		)
-		tmp, _ := teamKeyboard(gid)
-		msg.ReplyMarkup = tmp
-	default:
-		resp, err = bot.AnswerCallbackQuery(
-			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Unknown Callback"},
-		)
-	}
-
-	if err != nil {
-		wasabi.Log.Error(err)
-		return msg, err
-	}
-	if !resp.Ok {
-		wasabi.Log.Error(resp.Description)
-	}
-	return msg, nil
-}
-
-func callbackTeam(action, team string, gid wasabi.GoogleID, lang string, msg *tgbotapi.MessageConfig) error {
-	type tStruct struct {
-		State string
-		Team  string
-	}
-
-	t := wasabi.TeamID(team)
-	name, _ := t.Name()
-
-	switch action {
-	case "primary":
-		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
-			State: "Primary",
-			Team:  name,
-		})
-		gid.SetTeamState(t, "Primary")
-	case "activate":
-		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
-			State: "On",
-			Team:  name,
-		})
-		gid.SetTeamState(t, "On")
-	case "deactivate":
-		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
-			State: "Off",
-			Team:  name,
-		})
-		gid.SetTeamState(t, "Off")
-	default:
-		wasabi.Log.Error("Unknown Team command")
-	}
 	return nil
 }
 
---
+++ Telegram/callback.go
@@ -0,0 +1,153 @@
+package wasabitelegram
+
+import (
+	// "encoding/json"
+	"github.com/cloudkucooland/WASABI"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"strconv"
+	"strings"
+)
+
+func teamKeyboard(gid wasabi.GoogleID) (tgbotapi.InlineKeyboardMarkup, error) {
+	var ud wasabi.AgentData
+	var rows [][]tgbotapi.InlineKeyboardButton
+
+	if err := gid.GetAgentData(&ud); err == nil {
+		var i int
+		for _, v := range ud.Teams {
+			i++
+			var row []tgbotapi.InlineKeyboardButton
+			var on, off, primary tgbotapi.InlineKeyboardButton
+			if v.State == "Off" {
+				on = tgbotapi.NewInlineKeyboardButtonData("Activate "+v.Name, "team/activate/"+v.ID)
+				row = append(row, on)
+			}
+			if v.State == "On" || v.State == "Primary" {
+				off = tgbotapi.NewInlineKeyboardButtonData("Deactivate "+v.Name, "team/deactivate/"+v.ID)
+				row = append(row, off)
+			}
+			if v.State == "On" {
+				primary = tgbotapi.NewInlineKeyboardButtonData("Make "+v.Name+" Primary", "team/primary/"+v.ID)
+				row = append(row, primary)
+			}
+			rows = append(rows, row)
+
+			if i > 8 { // too many rows and the screen fills up
+				break
+			}
+		}
+	}
+
+	tmp := tgbotapi.InlineKeyboardMarkup{
+		InlineKeyboard: rows,
+	}
+	return tmp, nil
+}
+
+// callback is where to determine which callback is called, and what to do with it
+func callback(update *tgbotapi.Update) (tgbotapi.MessageConfig, error) {
+	var resp tgbotapi.APIResponse
+	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
+	lang := update.CallbackQuery.Message.From.LanguageCode
+	tgid := wasabi.TelegramID(update.CallbackQuery.From.ID)
+	gid, _, err := tgid.GidV() // can they be not verified?
+	if err != nil {
+		wasabi.Log.Error(err)
+		return msg, err
+	}
+
+	// s, _ := json.MarshalIndent(update.CallbackQuery, "", " " )
+	// wasabi.Log.Debug(string(s))
+
+	if update.CallbackQuery.Message.Location != nil && update.CallbackQuery.Message.Location.Latitude != 0 {
+		gid.AgentLocation(
+			strconv.FormatFloat(update.CallbackQuery.Message.Location.Latitude, 'f', -1, 64),
+			strconv.FormatFloat(update.CallbackQuery.Message.Location.Longitude, 'f', -1, 64),
+			"Telegram",
+		)
+		msg.Text = "Location Processed"
+	}
+
+	if update.CallbackQuery.Message.Chat.Type != "private" {
+		wasabi.Log.Errorf("Not in private chat: %s", update.CallbackQuery.Message.Chat.Type)
+		return msg, nil
+	}
+
+	// data is in format class/action/id e.g. "team/deactivate/wibbly-wobbly-9988"
+	command := strings.SplitN(update.CallbackQuery.Data, "/", 3)
+	switch command[0] {
+	case "team":
+		_ = callbackTeam(command[1], command[2], gid, lang, &msg)
+		resp, err = bot.AnswerCallbackQuery(
+			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Team Updated", ShowAlert: false},
+		)
+		tmp, _ := teamKeyboard(gid)
+		msg.ReplyMarkup = tmp
+	case "operation": // XXX nothing yet
+		_ = callbackOperation(command[1], command[2], gid, lang, &msg)
+		resp, err = bot.AnswerCallbackQuery(
+			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Operation not supported yet"},
+		)
+	case "target": // XXX nothing yet
+		_ = callbackTarget(command[1], command[2], gid, lang, &msg)
+		resp, err = bot.AnswerCallbackQuery(
+			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Target not supported yet"},
+		)
+	default:
+		resp, err = bot.AnswerCallbackQuery(
+			tgbotapi.CallbackConfig{CallbackQueryID: update.CallbackQuery.ID, Text: "Unknown Callback"},
+		)
+	}
+
+	if err != nil {
+		wasabi.Log.Error(err)
+		return msg, err
+	}
+	if !resp.Ok {
+		wasabi.Log.Error(resp.Description)
+	}
+	return msg, nil
+}
+
+func callbackTeam(action, team string, gid wasabi.GoogleID, lang string, msg *tgbotapi.MessageConfig) error {
+	type tStruct struct {
+		State string
+		Team  string
+	}
+
+	t := wasabi.TeamID(team)
+	name, _ := t.Name()
+
+	switch action {
+	case "primary":
+		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
+			State: "Primary",
+			Team:  name,
+		})
+		gid.SetTeamState(t, "Primary")
+	case "activate":
+		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
+			State: "On",
+			Team:  name,
+		})
+		gid.SetTeamState(t, "On")
+	case "deactivate":
+		msg.Text, _ = templateExecute("TeamStateChange", lang, tStruct{
+			State: "Off",
+			Team:  name,
+		})
+		gid.SetTeamState(t, "Off")
+	default:
+		wasabi.Log.Error("Unknown Team command")
+	}
+	return nil
+}
+
+func callbackOperation(action, op string, gid wasabi.GoogleID, lang string, msg *tgbotapi.MessageConfig) error {
+	return nil
+}
+
+func callbackTarget(action, target string, gid wasabi.GoogleID, lang string, msg *tgbotapi.MessageConfig) error {
+	// XXX mark completed, what else?
+	return nil
+}
