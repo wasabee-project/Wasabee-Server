6ab366923fe7f2d09b729020e9405a48be940bbb
Scot Bontrager <scot@indievisible.org>
2019-05-02 14:30:40 -0500

    add gm test

---
+++ model_groupme_test.go
@@ -0,0 +1,18 @@
+package wasabi_test
+
+import (
+	"github.com/cloudkucooland/WASABI"
+	"testing"
+)
+
+// nothing to do here, just run the code
+func testGM(t *testing.T) {
+	wasabi.GMSetBot()
+	b, err := wasabi.GMRunning()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if !b {
+		t.Errorf(err.Error())
+	}
+}
