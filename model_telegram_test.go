4837048bf4087b09e86c6ccc3c13a05487339124
Scot Bontrager <scot@indievisible.org>
2019-05-02 14:42:08 -0500

    easy tg tests

---
+++ model_telegram_test.go
@@ -0,0 +1,31 @@
+package wasabi_test
+
+import (
+	"github.com/cloudkucooland/WASABI"
+	"testing"
+)
+
+func TestTG(t *testing.T) {
+	tgID, err := gid.TelegramID()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	ngid, v, err := tgID.GidV()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if !v {
+		t.Error("gid should be verified")
+	}
+	if ngid != gid {
+		t.Error("gid -> tgID -> gid did not roundtrip correctly")
+	}
+	_ = tgID.String()
+
+	_, err = wasabi.TGGetBotID()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// init/verify/delete bogus acccount
+}
