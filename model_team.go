91451a06f96fcd56c5abd27d7d74976bed232c10
Scot Bontrager <scot@indievisible.org>
2019-07-14 21:23:53 -0500

    format

--- model_team.go
+++ model_team.go
@@ -12,11 +12,11 @@ import (
 
 // TeamData is the wrapper type containing all the team info
 type TeamData struct {
-	Name      string   `json:"name"`
-	ID        TeamID   `json:"id"`
-	Agent     []Agent  `json:"agents"`
-	RocksComm string   `json:"rc"`
-	RocksKey  string   `json:"rk"`
+	Name      string  `json:"name"`
+	ID        TeamID  `json:"id"`
+	Agent     []Agent `json:"agents"`
+	RocksComm string  `json:"rc"`
+	RocksKey  string  `json:"rk"`
 }
 
 // Agent is the light version of AgentData, containing visible information exported to teams
