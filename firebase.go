1efc09c1c3a5c5d9cfafd61215c248cbb4c2d049
Scot Bontrager <scot@indievisible.org>
2019-08-02 19:59:39 -0500

    first draft of firebase support

--- firebase.go
+++ firebase.go
@@ -9,8 +9,10 @@ var fb struct {
 	c       chan FirebaseCmd
 }
 
+// FirebaseCommandCode is the command codes used for communicating with the Firebase module
 type FirebaseCommandCode int
 
+// FbccQuit et al. are the friendly names for the FirebaseCommandCode
 const (
 	FbccQuit FirebaseCommandCode = iota
 	FbccGenericMessage
@@ -20,8 +22,10 @@ const (
 	FbccMarkerAssignmentChange
 	FbccLinkStatusChange
 	FbccLinkAssignmentChange
+	FbccSubscribeTeam
 )
 
+// FirebaseCmd is the struct passed to the Firebase module to take actions -- required params depend on the FBCC
 type FirebaseCmd struct {
 	Cmd    FirebaseCommandCode
 	TeamID TeamID
@@ -53,44 +57,49 @@ func (cc FirebaseCommandCode) String() string {
 }
 
 // Functions called from Wasabee to message the firebase subsystem
-func firebaseGeneric(cc FirebaseCommandCode, teamID TeamID, opID OperationID, objID string, gid GoogleID, msg string) {
+// func fbPush(cc FirebaseCommandCode, teamID TeamID, opID OperationID, objID string, gid GoogleID, msg string) {
+func fbPush(fbc FirebaseCmd) {
 	if !fb.running {
 		Log.Debug("Firebase is not running, not sending msg")
 		return
 	}
-
-	var fbc = FirebaseCmd{
-		Cmd:    cc,
-		TeamID: teamID,
-		OpID:   opID,
-		ObjID:  objID,
-		Gid:    gid,
-		Msg:    msg,
-	}
+	// Log.Debugf("sending %s", fbc.Cmd.String())
+	// XXX other sanity checking here?
 	fb.c <- fbc
 }
 
+// notifiy all subscribers to the agent's teams that they have moved
+// do not share the location since it is possible to subscribe to firebase topics without being on the team
 func (gid GoogleID) firebaseAgentLocation() {
 	if !fb.running {
 		return
 	}
 
-	// notify every team the agent has enabled
-	// do not share the location since it is possible to subscribe to firebase topics without being on the team
-	// XXX it is possible to send to a list of topics (teams) -- save on multiple sends to google...
-	for _, t := range gid.teamList() {
-		firebaseGeneric(FbccAgentLocationChange, t, "", "", gid, "")
+	for _, tid := range gid.teamList() {
+		fbPush(FirebaseCmd{
+			Cmd:    FbccAgentLocationChange,
+			TeamID: tid,
+			Gid:    gid,
+		})
 	}
 }
 
+// notifiy the agent that they have a new assigned marker in a given op
 func (opID OperationID) firebaseAssignMarker(gid GoogleID, markerID MarkerID) {
 	if !fb.running {
 		return
 	}
 
-	firebaseGeneric(FbccMarkerAssignmentChange, "", opID, string(markerID), gid, "assigned")
+	fbPush(FirebaseCmd{
+		Cmd:   FbccMarkerAssignmentChange,
+		OpID:  opID,
+		ObjID: string(markerID),
+		Gid:   gid,
+		Msg:   "assigned",
+	})
 }
 
+// notify a team that a marker's status has changed
 func (opID OperationID) firebaseMarkerStatus(markerID MarkerID, status string) {
 	if !fb.running {
 		return
@@ -101,7 +110,96 @@ func (opID OperationID) firebaseMarkerStatus(markerID MarkerID, status string) {
 		Log.Error(err)
 		return
 	}
-	firebaseGeneric(FbccMarkerStatusChange, teamID, opID, string(markerID), "", status)
+	fbPush(FirebaseCmd{
+		Cmd:    FbccMarkerStatusChange,
+		TeamID: teamID,
+		OpID:   opID,
+		ObjID:  string(markerID),
+		Msg:    status,
+	})
+}
+
+// notifiy the agent that they have a new assigned marker in a given op
+func (opID OperationID) firebaseAssignLink(gid GoogleID, linkID LinkID) {
+	if !fb.running {
+		return
+	}
+
+	fbPush(FirebaseCmd{
+		Cmd:   FbccLinkAssignmentChange,
+		OpID:  opID,
+		ObjID: string(linkID),
+		Gid:   gid,
+		Msg:   "assigned",
+	})
+}
+
+func (opID OperationID) firebaseLinkStatus(linkID LinkID, completed bool) {
+	if !fb.running {
+		return
+	}
+
+	msg := "complete"
+	if !completed {
+		msg = "incomplete"
+	}
+
+	teamID, err := opID.GetTeamID()
+	if err != nil {
+		Log.Error(err)
+		return
+	}
+	fbPush(FirebaseCmd{
+		Cmd:    FbccLinkStatusChange,
+		TeamID: teamID,
+		OpID:   opID,
+		ObjID:  string(linkID),
+		Msg:    msg,
+	})
+}
+
+func (opID OperationID) firebaseMapChange() {
+	if !fb.running {
+		return
+	}
+
+	teamID, err := opID.GetTeamID()
+	if err != nil {
+		Log.Error(err)
+		return
+	}
+	fbPush(FirebaseCmd{
+		Cmd:    FbccMapChange,
+		TeamID: teamID,
+		OpID:   opID,
+		Msg:    "changed",
+	})
+}
+
+func (gid GoogleID) firebaseSubscribeTeam(teamID TeamID) {
+	if !fb.running {
+		return
+	}
+
+	fbPush(FirebaseCmd{
+		Cmd:    FbccSubscribeTeam,
+		Gid:    gid,
+		TeamID: teamID,
+		Msg:    "subscribe",
+	})
+}
+
+func (gid GoogleID) firebaseUnsubscribeTeam(teamID TeamID) {
+	if !fb.running {
+		return
+	}
+
+	fbPush(FirebaseCmd{
+		Cmd:    FbccSubscribeTeam,
+		Gid:    gid,
+		TeamID: teamID,
+		Msg:    "unsubscribe",
+	})
 }
 
 // Functions called from Firebase to use Wasabee resources
@@ -117,4 +215,14 @@ func (gid GoogleID) FirebaseToken() (string, error) {
 	}
 
 	return token, nil
+}
+
+// FirebaseInsertToken updates a token in the database for an agent
+func (gid GoogleID) FirebaseInsertToken(token string) error {
+	_, err := db.Exec("INSERT INTO firebase (gid, token) VALUES (?, ?) ON DUPLICATE UPDATE token = ?", gid, token, token)
+	if err != nil {
+		Log.Error(err)
+		return err
+	}
+	return nil
 }
