446793677eaf841305329373b2e668662ac64906
Scot Bontrager <scot@indievisible.org>
2019-08-07 19:23:02 -0500

    sexy oneliner found while trying to rebuild this function in flutter/dart for the app

--- model_draw_links.go
+++ model_draw_links.go
@@ -250,26 +250,10 @@ func MinPortalLevel(distance float64, agents int, allowmods bool) float64 {
 		return 8.0
 	}
 
-	m := (root(distance, 4)) / (2 * root(10, 4))
+	m := (fourthroot(distance)) / (2 * fourthroot(10))
 	return m
 }
 
-func root(a float64, n int) float64 {
-	n1 := n - 1
-	n1f, rn := float64(n1), 1/float64(n)
-	x, x0 := 1., 0.
-	for {
-		potx, t2 := 1/x, a
-		for b := n1; b > 0; b >>= 1 {
-			if b&1 == 1 {
-				t2 *= potx
-			}
-			potx *= potx
-		}
-		x0, x = x, rn*(n1f*x+t2)
-		if math.Abs(x-x0)*1e15 < x {
-			break
-		}
-	}
-	return x
+func fourthroot (a float64) float64 {
+	return math.Pow(math.E, math.Log(a) / 4.0);
 }
