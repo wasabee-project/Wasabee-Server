f2ed3a54684fc62a7cfd04d80c0aa0d765dae977
Scot Bontrager <scot@indievisible.org>
2019-08-15 20:13:12 -0500

    make this a bit more functional

--- cmd/response-provider/main.go
+++ cmd/response-provider/main.go
@@ -11,6 +11,8 @@ const port = "8445"
 
 var tokens map[string]GoogleData
 var gids map[string]GoogleData
+var rocks map[string]RocksAgent
+var vees map[string]vagent
 
 // what is returned when pretending to be google
 type GoogleData struct {
@@ -44,12 +46,12 @@ type vagent struct {
 }
 
 type RocksAgent struct {
-	Gid      string   `json:"gid"`
-	TGId     int64    `json:"tgid"`
-	Agent    string   `json:"agentid"`
-	Verified bool     `json:"verified"`
-	Smurf    bool     `json:"smurf"`
-	Fullname string   `json:"name"`
+	Gid      string `json:"gid"`
+	TGId     int64  `json:"tgid"`
+	Agent    string `json:"agentid"`
+	Verified bool   `json:"verified"`
+	Smurf    bool   `json:"smurf"`
+	Fullname string `json:"name"`
 }
 
 func main() {
@@ -59,6 +61,13 @@ func main() {
 	tokens = make(map[string]GoogleData)
 	// gid -> GoogleData
 	gids = make(map[string]GoogleData)
+	// gid -> RocksAgent
+	rocks = make(map[string]RocksAgent)
+	// gid -> vagent
+	vees = make(map[string]vagent)
+
+	// set up some default users to query against
+	preload()
 
 	// set up handlers for the URIs
 	http.HandleFunc("/GoogleToken", googleToken)
@@ -82,19 +91,41 @@ func googleToken(res http.ResponseWriter, req *http.Request) {
 	fmt.Printf("looking for GoogleData for token %s", token)
 	data, ok := tokens[token]
 	if !ok {
-		fmt.Printf("generating new GoogleData for token %s", token)
+		fmt.Printf("generating new data for token %s", token)
 		// generate new GoogleData with random values
 		// XXX make this actually random
 		gid := "12345"
 		data = GoogleData{
 			Gid:   gid,
-			Name:  "random",
-			Email: "random@example.com",
-			Pic:   "http://example.com/12345.jpg",
+			Name:  fmt.Sprintf("random %s inadequate", gid),
+			Email: fmt.Sprintf("%s@example.com", gid),
+			Pic:   fmt.Sprintf("http://example.com/%s.jpg", gid),
 		}
 		// stuff it into the maps for later usage
 		tokens[token] = data
 		gids[gid] = data
+		rocks[gid] = RocksAgent{
+			Gid:      gid,
+			TGId:     0,
+			Agent:    fmt.Sprintf("Barcode-%s", gid[1:4]),
+			Verified: true,
+			Smurf:    false,
+			Fullname: "Rando Inadiquo",
+		}
+		vees[gid] = vagent{
+			EnlID:       fmt.Sprintf("enl-%s", data.Gid),
+			Vlevel:      1,
+			Vpoints:     1,
+			Agent:       fmt.Sprintf("Barcode-%s", data.Gid[1:4]),
+			Level:       16,
+			Quarantine:  false,
+			Active:      true,
+			Blacklisted: false,
+			Verified:    true,
+			Flagged:     false,
+			Banned:      false,
+			Cellid:      "AMS-GOLF-06",
+		}
 	}
 
 	j, _ := json.Marshal(data)
@@ -117,33 +148,18 @@ func vSearch(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(res, "gid not set")
 		return
 	}
-	data, ok := gids[gid]
 	var v Vresult
+	vd, ok := vees[gid]
 	if !ok {
-		// mirror the error that V actually uses
 		v = Vresult{
 			Status:  "error",
 			Message: "Agent not found",
 		}
 	} else {
-		// XXX craft and send random response
 		v = Vresult{
 			Status: "ok",
 		}
-		v.Data = vagent{
-			EnlID:       fmt.Sprintf("enl-%s", data.Gid),
-			Vlevel:      1,
-			Vpoints:     1,
-			Agent:       fmt.Sprintf("Barcode-%s", data.Gid[1:4]),
-			Level:       16,
-			Quarantine:  false,
-			Active:      true,
-			Blacklisted: false,
-			Verified:    true,
-			Flagged:     false,
-			Banned:      false,
-			Cellid:      "AMS-GOLF-06",
-		}
+		v.Data = vd
 	}
 	j, _ := json.Marshal(v)
 	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -164,22 +180,75 @@ func rocksSearch(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(res, "gid not set")
 		return
 	}
-	data, ok := gids[gid]
-	var r RocksAgent
+	r, ok := rocks[gid]
 	if !ok {
-		// XXX mirror the error that Rocks actually uses
-	} else {
-		// XXX craft and send random response
-		r = RocksAgent{
-			Gid:      data.Gid,
-			TGId:     0,
-			Agent:    fmt.Sprintf("Barcode-%s", data.Gid[1:4]),
-			Verified: true,
-			Smurf:    false,
-			Fullname: "Rando Inadiquo",
-		}
+		// XXX get the error that Rocks actually uses
+		res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		fmt.Fprint(res, `{ status: "error" }`)
+		return
 	}
 	j, _ := json.Marshal(r)
 	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	fmt.Fprintf(res, string(j))
+}
+
+func preload() {
+	// deviousness
+	gids["118281765050946915735"] = GoogleData{
+		Gid:   "118281765050946915735",
+		Name:  "Scot Bontrager",
+		Email: "scot@example.com",
+		Pic:   "http://example.com/scot.jpg",
+	}
+	rocks["118281765050946915735"] = RocksAgent{
+		Gid:      "118281765050946915735",
+		TGId:     240908008,
+		Agent:    "deviousness",
+		Verified: true,
+		Smurf:    false,
+		Fullname: "Scot Bontrager",
+	}
+	vees["118281765050946915735"] = vagent{
+		EnlID:       "23e27f48a04e55d6ae89188d3236d769f6629718",
+		Vlevel:      3,
+		Vpoints:     100,
+		Agent:       "deviousness",
+		Level:       16,
+		Quarantine:  false,
+		Active:      true,
+		Blacklisted: false,
+		Verified:    true,
+		Flagged:     false,
+		Banned:      false,
+		Cellid:      "AMS-GOLF-06",
+	}
+	// bogus banned account
+	gids["1111"] = GoogleData{
+		Gid:   "1111",
+		Name:  "Bogus Banned",
+		Email: "banned@example.com",
+		Pic:   "http://example.com/banned.jpg",
+	}
+	rocks["1111"] = RocksAgent{
+		Gid:      "1111",
+		TGId:     0,
+		Agent:    "bannedbarcode",
+		Verified: false,
+		Smurf:    true,
+		Fullname: "Bogus Banned",
+	}
+	vees["1111"] = vagent{
+		EnlID:       "1111",
+		Vlevel:      0,
+		Vpoints:     0,
+		Agent:       "bogusbanned",
+		Level:       2,
+		Quarantine:  false,
+		Active:      false,
+		Blacklisted: true,
+		Verified:    false,
+		Flagged:     false,
+		Banned:      true,
+		Cellid:      "AMS-GOLF-06",
+	}
 }
