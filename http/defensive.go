f72d75952295c3769acff8c45d3258efa9d58846
Scot Bontrager <scot@indievisible.org>
2020-02-20 21:00:31 -0600

    add new

---
+++ http/defensive.go
@@ -0,0 +1,56 @@
+package wasabeehttps
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/wasabee-project/Wasabee-Server"
+	"strconv"
+	"net/http"
+)
+
+func getDefensiveKeys(res http.ResponseWriter, req *http.Request) {
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Notice(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	dkl, err := gid.ListDefensiveKeys();
+	if err != nil {
+		wasabee.Log.Notice(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Add("Content-Type", jsonType)
+	data, _ := json.Marshal(dkl)
+	fmt.Fprint(res, string(data))
+}
+
+func setDefensiveKey(res http.ResponseWriter, req *http.Request) {
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Notice(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	portalID := wasabee.PortalID(req.FormValue("portalID"))
+	capID := req.FormValue("capID")
+	count, err := strconv.ParseInt(req.FormValue("count"), 10, 32)
+	if err != nil {
+		wasabee.Log.Notice(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = gid.InsertDefensiveKey(portalID, capID, int32(count))
+	if err != nil {
+		wasabee.Log.Notice(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res.Header().Add("Content-Type", jsonType)
+	fmt.Fprint(res, jsonStatusOK)
+}
---
+++ model_defense_keys.go
@@ -0,0 +1,52 @@
+package wasabee
+
+import (
+	"database/sql"
+)
+
+// DefensiveKeyList is the list of all defensive keys
+type DefensiveKeyList struct {
+	DefensiveKeys []DefensiveKey
+	Fetched       string
+}
+
+// AdOwnedTeam is a sub-struct of AgentData
+type DefensiveKey struct {
+	GID      GoogleID
+	PortalID PortalID
+	CapID    string
+	Count    int32
+}
+
+// ListDefensiveKeys gets all keys an agent is authorized to know about.
+func (gid GoogleID) ListDefensiveKeys() (DefensiveKeyList, error) {
+	var dkl DefensiveKeyList
+
+	rows, err := db.Query("SELECT gid, portalID, capID, count FROM defensivekeys WHERE gid IN (SELECT DISTINCT x.gid FROM agentteams=x, agentteam=y WHERE y.gid = ? AND y.state = 'On' AND x.teamID = y.teamID AND x.state = 'On')", gid)
+	if err != nil && err != sql.ErrNoRows {
+		Log.Notice(err)
+		return dkl, err
+	}
+	defer rows.Close()
+	var dk DefensiveKey
+	for rows.Next() {
+		err := rows.Scan(&dk.GID, &dk.PortalID, &dk.CapID, &dk.Count)
+		if err != nil {
+			Log.Notice(err)
+			return dkl, err
+		}
+		dkl.DefensiveKeys = append(dkl.DefensiveKeys, dk)
+	}
+	// XXX set fetched time
+	return dkl, nil
+}
+
+func (gid GoogleID) InsertDefensiveKey(portalID PortalID, capID string, count int32) error {
+	// XXX if count = 0, delete
+
+	if _, err := db.Exec("INSERT INTO defensivekeys (gid, portalID, capID, count) VALUES (?, ?, ?, ?) ON DULPICATE UPDATE capID = ?, count = ?", gid, portalID, capID, count, capID, count); err != nil {
+		Log.Notice(err)
+		return err
+	}
+	return nil
+}
