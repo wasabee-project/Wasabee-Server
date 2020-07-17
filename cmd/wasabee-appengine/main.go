deb09b658e1df9dd35b240bd353fde3a74e2410c
Scot Bontrager <scot@indievisible.org>
2020-07-17 12:56:38 -0500

    still not working, but better

--- cmd/wasabee-appengine/main.go
+++ cmd/wasabee-appengine/main.go
@@ -1,42 +1,77 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
-	"path"
-	// "strings"
 	"syscall"
+
+	// "cloud.google.com/go"
+	"cloud.google.com/go/storage"
 
 	"github.com/wasabee-project/Wasabee-Server"
 	"github.com/wasabee-project/Wasabee-Server/Firebase"
-	"github.com/wasabee-project/Wasabee-Server/RISC"
+	// "github.com/wasabee-project/Wasabee-Server/RISC"
+	"github.com/wasabee-project/Wasabee-Server/PubSub"
 	"github.com/wasabee-project/Wasabee-Server/Telegram"
 	"github.com/wasabee-project/Wasabee-Server/http"
-	"github.com/wasabee-project/Wasabee-Server/PubSub"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
 func main() {
-	certs := "./certs/"
+	// project := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	bucket := os.Getenv("BUCKET")
 	frontend := os.Getenv("FRONTEND_PATH")
+	words := os.Getenv("WORDS_PATH")
+	creds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-	// Load words
-	err := wasabee.LoadWordsFile("word.txt")
+	wasabee.Log.Infof("Using Creds: %s", creds);
+
+	ctx := context.Background()
+
+	wasabee.Log.Info("Loading Word List")
+	// connect using the App Engine's creds
+	storageClient, err := storage.NewClient(ctx)
 	if err != nil {
-		wasabee.Log.Errorf("Error loading word list from '%s': %s", "word.txt", err)
+		wasabee.Log.Errorf("Error initializng storage: %s", err)
 	}
-
-	// load the UI templates
-	ts, err := wasabee.TemplateConfig(frontend)
+	b := storageClient.Bucket(bucket)
+	w := b.Object(words)
+	r, err := w.NewReader(ctx)
 	if err != nil {
-		wasabee.Log.Errorf("unable to load frontend templates from %s; shutting down", frontend)
+		wasabee.Log.Errorf("Error loading word list from cloud storage: %s/%s: %s", bucket, words, err)
+		panic(err)
+	}
+	err = wasabee.LoadWordsStream(r)
+	if err != nil {
+		wasabee.Log.Errorf("Error parsing list: %s", err)
 		panic(err)
 	}
 
+	wasabee.Log.Info("Loading UI Templates")
+	// load the UI templates
+	ts, err := wasabee.TemplateConfigAppengine(b, frontend)
+	if err != nil {
+		wasabee.Log.Errorf("unable to load frontend templates from %s/%s: %s ", bucket, frontend, err)
+		panic(err)
+	}
+
+	wasabee.Log.Info("Starting Firebase")
+	go wasabeefirebase.ServeFirebase(creds)
+	defer wasabee.FirebaseClose()
+	wasabee.Log.Info("Starting Pub/Sub")
+	go wasabeepubsub.StartPubSub(wasabeepubsub.Configuration{
+		Cert:    creds,
+		Project: "PhDevBin",
+		Topic:   "wasabee-main",
+	})
+	defer wasabee.PubSubClose()
+
 	// Connect to database
 	dbstring := os.Getenv("DATABASE")
+	wasabee.Log.Infof("Connecting to : %s", dbstring)
 	if dbstring == "" {
 		dbstring = "user:pass@tcp(localhost)/wasabee"
 	}
@@ -70,64 +105,36 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = ":8443"
+		port = ":8080"
 	}
 
 	sessionkey := os.Getenv("SESSION_KEY")
 	clientid := os.Getenv("OAUTH_CLIENT_ID")
 	clientsecret := os.Getenv("OAUTH_CLIENT_SECRET")
-	httpslog := "/dev/null"
 	root := os.Getenv("ROOT_URL")
 	if root == "" {
-		root = "https://zone.wasabee.rocks"
+		root = "https://cdn.wasabee.rocks"
 	}
 
-	// Serve HTTPS
-		go wasabeehttps.StartHTTP(wasabeehttps.Configuration{
-			ListenHTTPS:      port,
-			FrontendPath:     frontend,
-			Root:             root,
-			CertDir:          certs,
-			OauthConfig:  &oauth2.Config {
-				ClientID: clientid,
-				ClientSecret: clientsecret,
-				Scopes: []string{"profile email"},
-				Endpoint: oauth2.Endpoint {
-					AuthURL: google.Endpoint.AuthURL,
-					TokenURL: google.Endpoint.TokenURL,
-					AuthStyle: oauth2.AuthStyleInParams,
-				},
+	// Serve HTTP
+	go wasabeehttps.StartAppEngine(wasabeehttps.Configuration{
+		ListenHTTPS:  ":" + port,
+		FrontendPath: frontend,
+		Root:         root,
+		OauthConfig: &oauth2.Config{
+			ClientID:     clientid,
+			ClientSecret: clientsecret,
+			Scopes:       []string{"profile email"},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:   google.Endpoint.AuthURL,
+				TokenURL:  google.Endpoint.TokenURL,
+				AuthStyle: oauth2.AuthStyleInParams,
 			},
-			OauthUserInfoURL: "https://www.googleapis.com/oauth2/v2/userinfo",
-			CookieSessionKey: sessionkey,
-			Logfile:          httpslog,
-			TemplateSet:      ts,
-		})
-
-	riscPath := path.Join(certs, "risc.json")
-	if _, err := os.Stat(riscPath); err != nil {
-		wasabee.Log.Noticef("%s does not exist, not enabling RISC", riscPath)
-	} else {
-		go risc.RISC(riscPath)
-	}
-
-	firebasePath := path.Join(certs, "firebase.json")
-	if _, err := os.Stat(firebasePath); err != nil {
-		wasabee.Log.Noticef("%s does not exist, not enabling Firebase", firebasePath)
-	} else {
-		go wasabeefirebase.ServeFirebase(firebasePath)
-	}
-
-	pubsubPath := path.Join(certs, "pubsub.json")
-	if _, err := os.Stat(pubsubPath); err != nil {
-		wasabee.Log.Noticef("%s does not exist, not enabling PubSub", pubsubPath)
-	} else {
-		go wasabeepubsub.StartPubSub(wasabeepubsub.Configuration{
-			Cert: pubsubPath,
-			Project: "PhDevBin",
-			Topic: "wasabee-main",
-		})
-	}
+		},
+		OauthUserInfoURL: "https://www.googleapis.com/oauth2/v2/userinfo",
+		CookieSessionKey: sessionkey,
+		TemplateSet:      ts,
+	})
 
 	// Serve Telegram
 	tgkey := os.Getenv("TELEGRAM_API_KEY")
@@ -147,15 +154,6 @@ func main() {
 	sig := <-sigch
 
 	wasabee.Log.Info("Shutdown Requested: ", sig)
-	if _, err := os.Stat(firebasePath); err == nil {
-		wasabee.FirebaseClose()
-	}
-	if _, err := os.Stat(pubsubPath); err == nil {
-		wasabee.PubSubClose()
-	}
-	if _, err := os.Stat(riscPath); err == nil {
-		risc.DisableWebhook()
-	}
 	if r, _ := wasabee.TGRunning(); r {
 		wasabeetelegram.Shutdown()
 	}
