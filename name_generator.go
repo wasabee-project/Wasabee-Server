95c941fae0f84d2c7de9af5ba9de246342e12b71
scot@indievisible.org <scot@indievisible.org>
2020-09-02 01:00:34 +0000

    improve legacy qBin code

--- name_generator.go
+++ name_generator.go
@@ -12,18 +12,33 @@ import (
 var words = []string{}
 var characters = strings.Split("abcdefghijklmnopqrstuvwxyz0123456789", "")
 
-func randomWord(array []string, try int, err error) string {
+func randomWord(array []string, try int) string {
 	if try > 10 {
-		// We somehow tried 10 times to get a random value and it failed every time. Something is extremely wrong here.
-		Log.Fatalw("crypto/rand issue - something is probably extremely wrong!", "error", err)
-		// panic(err)
+		Log.Fatalw("crypto/rand failed 10 times, giving up")
+		return ""
 	}
 
 	i, err := rand.Int(rand.Reader, big.NewInt(int64(len(array))))
 	if err != nil {
-		return randomWord(array, try+1, err)
+		Log.Error(err)
+		return randomWord(array, try+1)
 	}
 	return array[i.Int64()]
+}
+
+// GenerateID generates a random ASCII-safe string of specified length
+func GenerateID(size int) string {
+	var buf = make([]byte, size)
+
+	for i := 0 ; i < size ; i++ {
+		r, err := rand.Int(rand.Reader, big.NewInt(int64(len(characters))))
+		if err != nil {
+			Log.Error(err)
+		}
+		b := []byte(characters[r.Int64()])
+		buf[i] = b[0]
+	}
+	return string(buf)
 }
 
 // LoadWordsFile imports the word definition file used for names.
@@ -76,25 +91,13 @@ func loadWords(content []byte) error {
 	return nil
 }
 
-// GenerateName generates a slug in the format "cornflake-peddling-bp0q". Note that this function will return an empty string if an error occurs along the way!
+// GenerateName generates a slug in the format "cornflake-peddling-bp0q".
 func GenerateName() string {
-	text := ""
-	var word string
-	for i := 0; i < 6; i++ {
-		if i < 2 && len(words) > 0 {
-			word = randomWord(words, 0, nil)
-		} else {
-			word = randomWord(characters, 0, nil)
-		}
+	first := randomWord(words, 0)
+	second := randomWord(words, 0)
+	third := GenerateID(5);
 
-		if i < 3 && len(words) > 0 {
-			text += "-" + word
-		} else {
-			text += word
-		}
-	}
-
-	return strings.TrimPrefix(text, "-")
+	return fmt.Sprintf("%s-%s-%s", first, second, third)
 }
 
 // GenerateSafeName generates a slug (like GenerateName()) that doesn't exist in the database yet.
