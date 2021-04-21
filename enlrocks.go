bc513517e0cc37eab65dafa5282662272cab7ba4
scot@indievisible.org <scot@indievisible.org>
2021-04-21 19:00:37 +0000

    more doppelkeks stuff

--- enlrocks.go
+++ enlrocks.go
@@ -146,7 +146,7 @@ func RocksUpdate(id AgentID, agent *RocksAgent) error {
 		// doppelkeks error
 		if err != nil && strings.Contains(err.Error(), "Error 1062") {
 			iname := fmt.Sprintf("%s-%s", agent.Agent, gid)
-			Log.Errorw("dupliate ingress agent name detected", "GID", agent.Agent, "new name", iname)
+			Log.Warnw("dupliate ingress agent name detected", "GID", agent.Agent, "new name", iname)
 			if _, err := db.Exec("UPDATE agent SET iname = ?, RocksVerified = ? WHERE gid = ?", iname, agent.Verified, gid); err != nil {
 				Log.Error(err)
 				return err
