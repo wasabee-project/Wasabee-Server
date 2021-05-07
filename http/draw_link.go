f0134765606d2569a1f3fd41750816fc20538d35
Scot Bontrager <scot@indievisible.org>
2021-05-07 13:07:25 -0500

    this was unwieldy

---
+++ http/draw_link.go
@@ -0,0 +1,309 @@
+package wasabeehttps
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/wasabee-project/Wasabee-Server"
+)
+
+func drawLinkAssignRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to assign agents")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+	link := wasabee.LinkID(vars["link"])
+	agent := wasabee.GoogleID(req.FormValue("agent"))
+	uid, err := op.AssignLink(link, agent)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	// wasabee.Log.Infow("assigned link", "GID", gid, "resource", op.ID, "link", link, "agent", agent, "message", "assigned link")
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkDescRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("write access required to set link descriptions")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+	link := wasabee.LinkID(vars["link"])
+	desc := req.FormValue("desc")
+	uid, err := op.LinkDescription(link, desc)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkColorRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to set link color")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+	link := wasabee.LinkID(vars["link"])
+	color := req.FormValue("color")
+	uid, err := op.LinkColor(link, color)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkSwapRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to swap link order")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	link := wasabee.LinkID(vars["link"])
+	uid, err := op.LinkSwap(link)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkZoneRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to set zone")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	link := wasabee.LinkID(vars["link"])
+	zone := wasabee.ZoneFromString(req.FormValue("zone"))
+
+	uid, err := link.SetZone(&op, zone)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkDeltaRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to set delta")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	link := wasabee.LinkID(vars["link"])
+	delta, err := strconv.ParseInt(req.FormValue("delta"), 10, 32)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	uid, err := op.LinkDelta(link, int(delta))
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkCompleteRoute(res http.ResponseWriter, req *http.Request) {
+	drawLinkCompRoute(res, req, true)
+}
+
+func drawLinkIncompleteRoute(res http.ResponseWriter, req *http.Request) {
+	drawLinkCompRoute(res, req, false)
+}
+
+func drawLinkCompRoute(res http.ResponseWriter, req *http.Request, complete bool) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	// write access OR asignee
+	link := wasabee.LinkID(vars["link"])
+	if !op.WriteAccess(gid) && !op.ID.AssignedTo(link, gid) {
+		err = fmt.Errorf("permission to mark link as complete denied")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	uid, err := op.LinkCompleted(link, complete)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawLinkFetch(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	// o.Populate determines all or assigned-only
+	read, _ := op.ReadAccess(gid)
+	if !read && !op.AssignedOnlyAccess(gid) {
+		if op.ID.IsDeletedOp() {
+			err := fmt.Errorf("requested deleted op")
+			wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+			http.Error(res, jsonError(err), http.StatusGone)
+			return
+		}
+
+		err := fmt.Errorf("forbidden")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	// populate the whole op, slow, but ensures we only get things we have access to see
+	if err = op.Populate(gid); err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	linkID := wasabee.LinkID(vars["link"])
+	link, err := op.GetLink(linkID)
+	if err != nil {
+		wasabee.Log.Error(err)
+		// not really a 404, but close enough, better than a 500 or 403
+		http.Error(res, jsonError(err), http.StatusNotFound)
+		return
+	}
+	j, err := json.Marshal(link)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, string(j))
+}
---
+++ http/draw_marker.go
@@ -0,0 +1,302 @@
+package wasabeehttps
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/wasabee-project/Wasabee-Server"
+)
+
+func drawMarkerAssignRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("write access required to assign targets")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	marker := wasabee.MarkerID(vars["marker"])
+	agent := wasabee.GoogleID(req.FormValue("agent"))
+	if err = op.Populate(gid); err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	uid, err := op.AssignMarker(marker, agent)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	wasabee.Log.Infow("assigned marker", "GID", gid, "resource", op.ID, "marker", marker, "agent", agent, "message", "assigned marker")
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerCommentRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("write access required to set marker comments")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	marker := wasabee.MarkerID(vars["marker"])
+	comment := req.FormValue("comment")
+	uid, err := op.MarkerComment(marker, comment)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerZoneRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("write access required to set marker zone")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	marker := wasabee.MarkerID(vars["marker"])
+	zone := wasabee.ZoneFromString(req.FormValue("zone"))
+	uid, err := marker.SetZone(&op, zone)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerDeltaRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	// only the ID needs to be set for this
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	if !op.WriteAccess(gid) {
+		err = fmt.Errorf("forbidden: write access required to set delta")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	marker := wasabee.MarkerID(vars["marker"])
+	delta, err := strconv.ParseInt(req.FormValue("delta"), 10, 32)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	uid, err := marker.Delta(&op, int(delta))
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerFetch(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+
+	// o.Populate determines all or assigned-only
+	read, _ := op.ReadAccess(gid)
+	if !read && !op.AssignedOnlyAccess(gid) {
+		if op.ID.IsDeletedOp() {
+			err := fmt.Errorf("requested deleted op")
+			wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+			http.Error(res, jsonError(err), http.StatusGone)
+			return
+		}
+
+		err := fmt.Errorf("forbidden")
+		wasabee.Log.Warnw(err.Error(), "GID", gid, "resource", op.ID)
+		http.Error(res, jsonError(err), http.StatusForbidden)
+		return
+	}
+
+	// populate the whole op, slow, but ensures we only get things we have access to see
+	if err = op.Populate(gid); err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	markerID := wasabee.MarkerID(vars["marker"])
+	marker, err := op.GetMarker(markerID)
+	if err != nil {
+		wasabee.Log.Error(err)
+		// not really a 404, but close enough, better than a 500 or 403
+		http.Error(res, jsonError(err), http.StatusNotFound)
+		return
+	}
+	j, err := json.Marshal(marker)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, string(j))
+}
+
+func drawMarkerCompleteRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+	markerID := wasabee.MarkerID(vars["marker"])
+	uid, err := markerID.Complete(op, gid)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerIncompleteRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+	markerID := wasabee.MarkerID(vars["marker"])
+	uid, err := markerID.Incomplete(op, gid)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerRejectRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+	markerID := wasabee.MarkerID(vars["marker"])
+	uid, err := markerID.Reject(&op, gid)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
+
+func drawMarkerAcknowledgeRoute(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", jsonType)
+	gid, err := getAgentID(req)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(req)
+	var op wasabee.Operation
+	op.ID = wasabee.OperationID(vars["document"])
+	markerID := wasabee.MarkerID(vars["marker"])
+	uid, err := markerID.Acknowledge(&op, gid)
+	if err != nil {
+		wasabee.Log.Error(err)
+		http.Error(res, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+	wasabee.Log.Infow("acknowledged marker", "GID", gid, "resource", op.ID, "marker", markerID, "message", "acknowledged marker")
+	fmt.Fprint(res, jsonOKUpdateID(uid))
+}
