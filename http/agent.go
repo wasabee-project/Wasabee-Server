4f32f7025a15626023583d471487b26688a3046c
Scot Bontrager <scot@indievisible.org>
2021-06-27 15:34:29 -0500

    send FB even if tg fails

--- http/agent.go
+++ http/agent.go
@@ -7,7 +7,6 @@ import (
 	"github.com/wasabee-project/Wasabee-Server"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func agentProfileRoute(res http.ResponseWriter, req *http.Request) {
@@ -170,12 +169,6 @@ func agentTargetRoute(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if contentTypeIs(req, "multipart/form-data") {
-		wasabee.Log.Infow("using old format for sending targets", "GID", gid)
-		agentTargetRouteOld(res, req)
-		return
-	}
-
 	if !contentTypeIs(req, jsonTypeShort) {
 		err := fmt.Errorf("must use content-type: %s", jsonTypeShort)
 		wasabee.Log.Errorw(err.Error(), "GID", gid)
@@ -264,7 +257,6 @@ func agentTargetRoute(res http.ResponseWriter, req *http.Request) {
 		// do not report send errors up the chain, just log
 	}
 
-	// XXX can send to anyone -- make "shared enabled team"
 	ok, err := togid.SendMessage(msg)
 	if err != nil {
 		wasabee.Log.Error(err)
@@ -273,94 +265,8 @@ func agentTargetRoute(res http.ResponseWriter, req *http.Request) {
 	}
 	if !ok {
 		err := fmt.Errorf("message did not send")
-		wasabee.Log.Warnw(err.Error(), "from", gid, "to", togid)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-		return
-	}
-
-	out, err := json.Marshal(templateData)
-	if err != nil {
-		wasabee.Log.Warnw(err.Error(), "raw", templateData)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-	}
-	togid.FirebaseTarget(string(out))
-	fmt.Fprint(res, jsonStatusOK)
-}
-
-func agentTargetRouteOld(res http.ResponseWriter, req *http.Request) {
-	res.Header().Add("Content-Type", jsonType)
-	gid, err := getAgentID(req)
-	if err != nil {
-		wasabee.Log.Error(err)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-		return
-	}
-
-	vars := mux.Vars(req)
-	id := vars["id"]
-	togid, err := wasabee.ToGid(id)
-	if err != nil {
-		wasabee.Log.Error(err)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-		return
-	}
-
-	portal := req.FormValue("portal")
-	if portal == "" {
-		err := fmt.Errorf("portal net set")
-		wasabee.Log.Warnw(err.Error(), "GID", gid)
-		http.Error(res, jsonError(err), http.StatusNotAcceptable)
-		return
-	}
-
-	ll := req.FormValue("ll")
-	if ll == "" {
-		err := fmt.Errorf("ll not set")
-		wasabee.Log.Warnw(err.Error(), "GID", gid)
-		http.Error(res, jsonError(err), http.StatusNotAcceptable)
-		return
-	}
-
-	lls := strings.Split(ll, ",")
-	lls = lls[:2] // make it be exactly 2 long
-
-	name, err := gid.IngressName()
-	if err != nil {
-		wasabee.Log.Error(err)
-	}
-
-	templateData := struct {
-		Name   string
-		Lat    string
-		Lon    string
-		Type   string
-		Sender string
-	}{
-		Name:   portal,
-		Lat:    lls[0],
-		Lon:    lls[1],
-		Type:   "ad-hoc target",
-		Sender: name,
-	}
-
-	msg, err := gid.ExecuteTemplate("target", templateData)
-	if err != nil {
-		wasabee.Log.Error(err)
-		msg = fmt.Sprintf("template failed: ad-hoc target @ %s", ll)
-		// do not report send errors up the chain, just log
-	}
-
-	ok, err := togid.SendMessage(msg)
-	if err != nil {
-		wasabee.Log.Error(err)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-		return
-	}
-	if !ok {
-		err := fmt.Errorf("message did not send")
-		wasabee.Log.Warnw(err.Error(), "from", gid, "to", togid)
-		http.Error(res, jsonError(err), http.StatusInternalServerError)
-		return
+		wasabee.Log.Infow(err.Error(), "from", gid, "to", togid, "msg", msg)
+		// continue and send via firebase
 	}
 
 	out, err := json.Marshal(templateData)
