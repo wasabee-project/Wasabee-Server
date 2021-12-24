9199132b5f6552472fb0730ec6cea95b1999af9a
Scot Bontrager <scot@indievisible.org>
2021-12-23 23:24:41 -0600

    fafjkla;fjdkla

--- Firebase/messaging.go
+++ Firebase/messaging.go
@@ -181,7 +181,6 @@ func TaskStatus(taskID model.TaskID, opID model.OperationID, teamID model.TeamID
 		log.Error(err)
 		return err
 	}
-	wasabee.Log.Debugw("marker status", "subsystem", "Firebase", "msg", data)
 	return nil
 }
 
