37ebf773ec2c4a81c169d71df7d8e9c17141b133
Scot Bontrager <scot@indievisible.org>
2022-01-24 21:18:14 -0600

    simplify sanitizer

--- util/sanitizer.go
+++ util/sanitizer.go
@@ -3,20 +3,24 @@ package util
 import (
 	"strings"
 
-	"github.com/microcosm-cc/bluemonday"
+	// "github.com/microcosm-cc/bluemonday"
 
 	"github.com/wasabee-project/Wasabee-Server/log"
 )
 
+/*
 var sanitizer *bluemonday.Policy
 
 func init() {
 	sanitizer = bluemonday.StrictPolicy()
 }
+*/
 
-// Sanitize removes all HTML/CSS from an input string, making it safe for re-display in an HTML/JS context
+// Sanitize is a very light-weight attempt to reduce XSS potential in well-behaved clients
 func Sanitize(in string) string {
-	out := strings.TrimSpace(sanitizer.Sanitize(in))
+	out := strings.Replace(in, "<", "&lt;", -1)
+	out = strings.Replace(out, ">", "&gt;", -1)
+	out = strings.TrimSpace(out)
 	if in != out {
 		log.Debugw("sanitize", "in", in, "out", out)
 	}
