2e5027b03c70762b0e589d09cf6f51660b9f599e
Scot Bontrager <scot@indievisible.org>
2022-02-09 19:49:08 -0600

    remove after testing

--- http/server.go
+++ http/server.go
@@ -92,16 +92,6 @@ func Start() {
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
-			// ClientAuth:               tls.RequireAndVerifyClientCert,
-			/* CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-			*/
 		},
 	}
 
