488af61dde1d535dffcb35af0e1199d87a9c27fa
Scot Bontrager <scot@indievisible.org>
2022-02-09 19:37:27 -0600

    SSL suites no longer needed, Go now takes care of this internally

--- http/server.go
+++ http/server.go
@@ -92,7 +92,8 @@ func Start() {
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
+			// ClientAuth:               tls.RequireAndVerifyClientCert,
+			/* CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
@@ -100,6 +101,7 @@ func Start() {
 				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
+			*/
 		},
 	}
 
